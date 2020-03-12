46e1943e8eca571620facdafbd18cf2c22d92e15
Admin <contact@admin.com>
2020-03-12 02:33:21 -0700

    initial commit for host-3

---
+++ wp-content/themes/makenzie-lite/archive.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Makenzie_Blog
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area small-12 large-9 cell">
+		<main id="main" class="site-main">
+			<?php
+				$makenzie_archive_title_on_off = makenzie_lite_get_theme_mod( 'archive_title_on_off', 'on' );
+				if ( $makenzie_archive_title_on_off != 'off') :
+					get_template_part( 'template-parts/header/template-title' );
+				endif;
+			?>
+		<?php
+		if ( have_posts() ) :
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+
+				the_post();
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_format() );
+
+			endwhile;
+
+			/* Custom post navigation */
+			makenzie_lite_pagination();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
